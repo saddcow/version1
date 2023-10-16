49be7494ad07357ce0a5cb64ec81564cb64afde4
Aisiah <aisantiago@gbox.adnu.edu.ph>
2023-10-16 15:38:52 +0800

    hakdog

---
+++ lib/reports.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:try1/database_manager.dart';
+
+class Reports extends StatefulWidget {
+  const Reports({super.key});
+
+  @override
+  State<Reports> createState() => _ReportsState();
+}
+
+class _ReportsState extends State<Reports> {
+  List dataList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Demo"),
+      ),
+      body: FutureBuilder(
+        future: FireStoreDataBase().getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text(
+              "Something went wrong",
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            dataList = snapshot.data as List;
+            return buildItems(dataList);
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildItems(dataList) => ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: dataList.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(
+            dataList[index]["Name"],
+          ),
+          subtitle:  Text(dataList[index]["Dept"]),
+          trailing: Text(
+            dataList[index]["RollNo"],
+          ),
+        );
+      });
+}
